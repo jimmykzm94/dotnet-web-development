084e128a7e7ef2d1914809694c87ed9b804c0084
Jimmy Kueh <jimmykzm94@gmail.com>
2022-06-20 12:29:46 +0800

    Add react demonstrate app only

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 This is the repository for .NET web development.
 
 First project: Web API (api1)
+Adding simple react app - not integrate to .NET (demonstrate only)
---
+++ app1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Weather from './Weather/weather'
+import TempInput from './UserInput/tempInput'
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+      <TempInput />
+    </div>
+  );
+}
+
+export default App;
